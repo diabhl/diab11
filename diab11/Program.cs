0aef491a7633c00367a7e6302bae12baebdaa0be
MAX-PRO <MAX-PRO@DESKTOP-C8DGSR5>
2023-03-07 15:18:29 +0200

    Add project files.

---
+++ diab11/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Drawing;
+
+List<string> dinosaurs = new List<string>();
+
+Console.WriteLine("\nCapacity: {0}", dinosaurs.Capacity);
+
+dinosaurs.Add("Tyrannosaurus");
+dinosaurs.Add("Amargasaurus");
+dinosaurs.Add("Mamenchisaurus");
+dinosaurs.Add("Deinonychus");
+dinosaurs.Add("Compsognathus");
+Console.WriteLine();
+foreach (string dinosaur in dinosaurs)
+{
+    Console.WriteLine(dinosaur);
+}
